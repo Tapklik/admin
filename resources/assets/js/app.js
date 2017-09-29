24bc2035a64b1ab2321e573563a8b9fbe96277b8
Rok Nemet <rok@roknemet.me>
2017-09-29 15:36:30 +0200

    Fix endpoint
    This is what happens when you fucking hurry.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -34,7 +34,7 @@ Vue.component('auth', require('./components/Auth.vue'));
 const app = new Vue({
     el: '#app',
     data: {
-        api : 'http://local.api.tapklik.com/v1/',
+        api : 'http://45.32.157.221:10006/v1/',
         path: '//api.tapklik.com/',
         token: false,
         config: {}
