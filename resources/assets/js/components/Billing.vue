036aa8490847d97bd3ee96cfff97439f981c4c86
EmirSila <emirsilajdzic16@gmail.com>
2017-12-15 10:52:49 +0100

    Fix code in Billing.vue

--- resources/assets/js/components/Billing.vue
+++ resources/assets/js/components/Billing.vue
@@ -1,57 +1,84 @@
 <template>
     <div>
+
+        <!-- HEADER START -->
         <div class="row">
             <div class="col-md-8">
                 <h1 class="title pull-left">Bills</h1>
-
-                <button class="btn btn-default pull-right" @click="openCreateBill()">
+                <button 
+                class="btn btn-default pull-right" 
+                @click="openCreateNewBill()"
+                >
                     <i class="fa fa-plus"></i> Create new bill
                 </button>
             </div>
             <div class="col-md-4">
-                <input type="search" class="form-control" placeholder="Search bill id..." v-model="search"/>
+                <input 
+                type="search" 
+                class="form-control" 
+                placeholder="Search bill id..." 
+                v-model="search_bills"
+                />
             </div>
         </div>
+        <!-- HEADER END -->
+
         <hr/>
+
+        <!-- BILLS START -->
         <table class="table table-striped">
             <thead>
             <tr>
                 <th>ID</th>
-                <th>Credit or Debit</th>
-                <th>Date of Payment</th>
+                <th>Credit/Debit</th>
+                <th>Date</th>
                 <th>Amount</th>
-                <th>Method of Payment</th>
+                <th>Method</th>
             </tr>
             </thead>
             <tbody class="vcenter">
-            <tr v-show="loading == true">
+
+            <!-- TABLE LOADER START -->
+            <tr v-if="bills_table_loading">
                 <td colspan="11" class="loader text-center">
                     <i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
                 </td>
             </tr>
-            <tr v-show="noresult">
+            <!-- TABLE LOADER END -->
+            
+            <!-- EMPTY TABLE MESSAGE START -->
+            <tr v-else-if="bills_table_empty">
                 <td colspan="11">
                     Sorry but theres nothing here... yet :)
                 </td>
             </tr>
-
-            <tr v-for="bill in bills">
+            <!-- EMPTY TABLE MESSAGE END -->
+            
+            <tr v-else v-for="bill in filtered_bills">
                 <td> {{ bill.id }} </td>
-                <td v-if="bill.debit == 0">Credit</td>
-                <td v-else>Debit</td>
+                <td> {{ bill.debit ? 'Debit' : 'Credit' }}</td>
                 <td> {{ bill.timestamp }} </td>
-                <td v-if="bill.debit == 0"> {{ $root.fromMicroDollars(bill.credit) }} </td>
-                <td v-else> {{ $root.fromMicroDollars(bill.debit) }} </td>
-                <td> {{bill.description}} </td>
+                <td> {{ bill.debit ? $root.fromMicroDollars(bill.debit) : $root.fromMicroDollars(bill.credit) }} </td>
+                <td> {{ bill.description }} </td>
             </tr>
             </tbody>
         </table>
+        <!-- BILLS END -->
+
+        <!-- MODALS START -->
+        <!-- CREATE NEW BILL START -->
         <div class="modal fade" id="_modal-create-new-bill" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                                aria-hidden="true">&times;</span></button>
+                        <button 
+                        type="button" 
+                        class="close" 
+                        data-dismiss="modal" 
+                        aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                         <h4 class="modal-title">Create New Bill</h4>
                     </div>
                     <div class="modal-body">
@@ -61,12 +88,21 @@
                                     <div class="col-xs-12 col-md-6">
                                         <label for="label-city">Amount</label>
                                         <br/>
-                                        <input type="text" class="form-control" v-model="bill.amount" id="label-city" />
+                                        <input 
+                                        type="text" 
+                                        class="form-control" 
+                                        v-model="new_bill.amount" 
+                                        id="label-city" 
+                                        />
                                     </div>
                                     <div class="col-xs-12 col-md-6">
                                         <label for="label-city">Tran Number</label>
                                         <br/>
-                                        <input type="text" class="form-control" id="label-city" />
+                                        <input 
+                                        type="text" 
+                                        class="form-control" 
+                                        id="label-city" 
+                                        />
                                     </div>
                                 </div>
                             </div>
@@ -75,7 +111,11 @@
                                     <div class="col-xs-12">
                                         <label for="label-approved">Method of Payment</label>
                                         <br/>
-                                        <select class="form-control" id="label-approved" v-model="bill.description">
+                                        <select 
+                                        class="form-control" 
+                                        id="label-approved" 
+                                        v-model="new_bill.description"
+                                        >
                                             <option value="cc payment">Credit Card</option>
                                             <option value="paypal payment">Paypal</option>
                                             <option value="bank transfer">Bank Transfer</option>
@@ -87,30 +127,54 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary" @click="createNewBill()">Create</button>
+                        <button 
+                        type="button" 
+                        class="btn btn-default" 
+                        data-dismiss="modal"
+                        >
+                            Close
+                        </button>
+                        <button 
+                        type="button" 
+                        class="btn btn-primary" 
+                        data-dismiss="modal" 
+                        @click="createNewBill()"
+                        >
+                            Create
+                        </button>
                     </div>
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
+                </div>
+            </div>
         </div>
+        <!-- CREATE NEW BILL END -->
+        <!-- MODALS START -->
+
     </div>
 </template>
 
 <script>
     export default {
         mounted() {
+            this.getIds();
         },
 
         data() {
 
             return {
-                bills: [],
-                loading: false,
-                noresult: false,
+                //ESSENTIALS
                 token: this.token,
-                bill: {
+                search_bills: '',
+                account_id: this.getIds(),
+
+                //BILLS
+                bills: [],
+                bills_table_loading: true,
+                bills_table_empty: false,
+                
+                //CREATE NEW BILL
+                new_bill: {
                     invoice_id: 'TZ0001ARG125878',
-                    amount: 0,
+                    credit: 0,
                     description: 'cc payment',
                     type: 'billing'
                 }
@@ -119,104 +183,62 @@
 
         methods: {
 
-            openCreateBill() {
+            //OVERALL
+            getIds() {
+                var pathname = window.location.pathname;
+                var result = pathname.split("/");
+                return result[2];
+            },
+
+            //BILLS
+            fetchBills() {            
+                axios.get(
+                    this.$root.api + 'accounts/' + this.account_id + '/banker/main?type=billing', 
+                    this.$root.config
+                ).then(response => {
+                        this.bills_table_empty = response.data.data == '' ? true : false;
+                        this.bills = response.data.data;
+                        this.bills_table_loading = false;
+                    }, error => {
+                        this.bills_table_loading = false;
+                    }
+                );
+            },
+
+
+
+            //CREATE NEW BILL
+            openCreateNewBill() {
                 $('#_modal-create-new-bill').modal();
             },
 
-            fetchBills() {
-                
-                this.loading = true;
-                var self = this;
-                var accountIdDraft = window.location.pathname.replace('\/accounts\/', '');
-                var accountId = accountIdDraft.slice(0,(accountIdDraft.length - 8));
-            
-                axios.get(self.$root.api + 'accounts/' + accountId + '/banker/main?type=billing', self.$root.config).then(response => {
-                    self.bills = response.data.data;
-                    self.loading = false;
-                }, error => {
-                    console.log("heeey")
-                    swal('Error', error, 'error');
-                })
-            },
-            collectBill() {
-                return {
-                    invoice_id: 'TZ0001ARG125878',
-                    credit: this.$root.toMicroDollars(this.bill.amount),
-                    description: this.bill.description,
-                    type: "billing"                    
-                }
-            },
             createNewBill() {
-                this.loading = true;
-                var accountIdDraft = window.location.pathname.replace('\/accounts\/', '');
-                var accountId = accountIdDraft.slice(0,(accountIdDraft.length - 8));
+                this.bills_table_loading = true;
 
-                return axios.post(this.$root.api + 'accounts/' + accountId + '/banker/main', this.collectBill(), this.$root.config).then(response => {
-
-                    this.fetchBills();
-                    this.loading = false;
-                    this.closeModal();
-                }, error => {
-                    this.loading = false;
-                    this.closeModal();
-                });
-            },
-
-
-            closeModal() {
-                $('#_modal-create-new-bill').modal('close');
-            },
-
-            toggleStatus(id, status, index) {
-
-                status = (1 == status) ? 0 : 1;
-                this.campaigns.data[index].status = status;
-
-                axios.put(this.$root.api + 'campaigns/' + id + '/approve', {approved: status}, this.$root.config).then(response => {
-                    this.fetchCampaigns();
-                }, error => {
-                    console.log(error);
-                });
-            },
-
-            generateUri(type, id) {
-
-                var endpoint = '';
-
-                switch(type) {
-
-                    case 'campaigns':
-                        endpoint += 'campaigns';
-                    break;
-
-                    case 'api':
-                        endpoint += '//api.tapklik.com/v1/campaigns/' + id;
-                    break;
-
-                    case 'creatives':
-                        endpoint += '/campaigns/' + id + '/creatives';
-                    break;
-                }
-
-                return endpoint;
+                axios.post(
+                    this.$root.api + 'accounts/' + this.account_id + '/banker/main', 
+                    this.new_bill, 
+                    this.$root.config
+                ).then(response => {
+                        this.bills_table_loading = false;
+                        this.fetchBills();
+                    }, error => {
+                        this.bills_table_loading = false;
+                        this.fetchBills();
+                    }
+                );
             }
         },
 
         computed: {
-
-            filterByBills: function () {
-
-                if (typeof this.bills.data == 'undefined') return;
-
+            
+            filtered_bills() {
                 var self = this;
 
-                var results = this.bills.data.filter( function(item) {
-
-                    return item.id.toLowerCase().indexOf(self.search.toLowerCase()) > -1;
-                });
-
-                this.noresult = (!results.length) ? true : false;
-
+                var results = this.bills.filter(bill => 
+                    bill.id.indexOf(self.search_bills) != -1
+                );
+                this.bills_table_empty = results == '' ? true : false;
                 return results;
             }
         },
