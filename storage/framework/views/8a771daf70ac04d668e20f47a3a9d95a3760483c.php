3aa8cc9825f2de1f770e9b7c6e1bfb8dfa7333ca
halidm <halid.mousa@gmail.com>
2018-05-23 09:46:53 +0400

    change gitignore

--- storage/framework/views/8a771daf70ac04d668e20f47a3a9d95a3760483c.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('breadcrumbs'); ?>
-    Bid <?php echo e($id); ?>
-
-<?php $__env->stopSection(); ?>
-
-<?php $__env->startSection('content'); ?>
-<bid-show></bid-show>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.default', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
--- storage/framework/views/bcf59d158481698f44e728ef16b52d7b18a5278c.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('breadcrumbs'); ?>
-    Manage campaigns
-<?php $__env->stopSection(); ?>
-
-<?php $__env->startSection('content'); ?>
-<campaigns-all></campaigns-all>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.default', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
